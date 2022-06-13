737ce407b494398d9b5572d6e84b0aca165e7e59
Craneonz <onewaybreak@gmail.com>
2022-06-13 15:27:16 -0400

    06131527

---
+++ interDev2/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private GameObject startObject;
+    private Rigidbody2D startComponent;
+    private GameObject camera1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startObject=GameObject.Find("Circle");
+        startComponent=startObject.GetComponent<Rigidbody2D>();
+        startComponent.gravityScale=0;
+        camera1=GameObject.Find("Camera");
+        camera1.GetComponent<Camera>().enabled=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space)){
+            startComponent.gravityScale=1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag=="middleObject"){
+            GameObject middleObject=GameObject.FindWithTag("middleObject");
+            middleObject.GetComponent<Rigidbody2D>().gravityScale=1;
+            Camera.main.enabled=false;
+            camera1.GetComponent<Camera>().enabled=true;
+        }
+    }
+}
